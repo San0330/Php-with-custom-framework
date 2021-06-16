1702282440ad012d802fa02cae678b9bfb67faa8
Nepali-San <sanneupane1024@gmail.com>
2021-06-16 23:04:41 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\core;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Application();
+
+$app->router->get('/',function(){
+    return 'Hello world';
+});
+
+$app->run();

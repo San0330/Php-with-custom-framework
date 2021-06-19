36867b1e5e086c7dda3d96d2ae170dadaf96336c
Nepali-San <sanneupane1024@gmail.com>
2021-06-19 11:40:35 +0545

    updated with symfony request/response package

--- public/index.php
+++ public/index.php
@@ -8,10 +8,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application();
 
-$app->router->get('/', function () {
-    return 'Hello world';
-});
-
+$app->router->get('/', 'home');
 $app->router->get('/contact', 'contact');
 
 $app->run();

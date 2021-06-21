d583bd83157277806853e3b7f349fd5152082041
Nepali-San <sanneupane1024@gmail.com>
2021-06-21 21:45:50 +0545

    added AuthController

---
+++ controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controllers;
+
+use app\core\BaseController;
+
+class AuthController extends BaseController
+{
+    public function login()
+    {
+        return $this->render("login");
+    }
+
+    public function register()
+    {
+        return $this->render("register");
+    }
+}

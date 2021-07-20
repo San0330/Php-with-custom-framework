9c5be96744334874869879f30e2cb70b46c8526b
Nepali-San <sanneupane1024@gmail.com>
2021-07-20 19:43:04 +0545

    Obtaining value from register form to controller function

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace app\controllers;
 
 use app\core\BaseController;
+use symfony\Component\HttpFoundation\Request;
 
 class AuthController extends BaseController
 {
@@ -19,7 +20,21 @@ class AuthController extends BaseController
     }
 
 
-    public function handleRegister(){
+    public function handleRegister(Request $req){
+       $email = $req->get('email'); 
+       $firstName = $req->get("firstName");
+       $lastName = $req->get("lastName");
+       $password = $req->get("password");
+       $confirmPassword = $req->get("confirmPassword");
 
+       if($password !== $confirmPassword){
+           echo "Password do not match";
+           return;
+       }
+
+       echo $email;
+       echo $firstName;
+       echo $lastName;
+       echo $password;
     }
 }
